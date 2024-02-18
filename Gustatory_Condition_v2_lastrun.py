2f529bf0401983839a05333e673befc4e5973cb2
Diane Rohrer <120501144+DianeRohrer@users.noreply.github.com>
2024-02-18 12:54:48 -0500

    Instructions changes
    took out scrunch up nose on visual and added rating instructions for gustatory

--- Gustatory_Condition_v2_lastrun.py
+++ Gustatory_Condition_v2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on February 15, 2024, at 16:45
+    on Sun Feb 18 12:52:06 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -54,7 +54,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\simla\\Documents\\GitHub\\Sadism_Disgust_Code\\Gustatory_Condition_v2_lastrun.py',
+    originPath='/Users/themorrigan/Desktop/Sadism_Disgust_Code/Gustatory_Condition_v2_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
@@ -201,6 +201,23 @@ screen_clear = visual.TextStim(win=win, name='screen_clear',
 p_port_9 = parallel.ParallelPort(address='0x3FF8')
 p_port_18 = parallel.ParallelPort(address='0x3FF8')
 
+# --- Initialize components for Routine "Palatability" ---
+Palatability_Score = visual.Slider(win=win, name='Palatability_Score',
+    startValue=None, size=(1.0, 0.1), pos=(0, -0.2), units=None,
+    labels=("Worst thing I have ever tasted", "", "Not horrible but would not try again", "", "Pleasant tasting, would try again", "", "Best thing I have ever tasted"), ticks=(1, 2, 3, 4, 5, 6, 7), granularity=1.0,
+    style='rating', styleTweaks=(), opacity=None,
+    labelColor=[0.8, 0.8, 0.8], markerColor=[1.0, -1.0, 1.0], lineColor='White', colorSpace='rgb',
+    font='Open Sans', labelHeight=0.02,
+    flip=False, ori=0.0, depth=0, readOnly=False)
+p_port_17 = parallel.ParallelPort(address='0x3FF8')
+Instruction = visual.TextStim(win=win, name='Instruction',
+    text='Please rate how good or bad liquid you received was.',
+    font='Open Sans',
+    pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=-2.0);
+
 # --- Initialize components for Routine "Rinse" ---
 Rinse_text = visual.TextStim(win=win, name='Rinse_text',
     text='When you hear the tone start rinsing. Please grab a cup. You will hear a second tone to tell you to stop and spit. ',
@@ -225,16 +242,6 @@ Rinse_Spit = visual.TextStim(win=win, name='Rinse_Spit',
 p_port_10 = parallel.ParallelPort(address='0x3FF8')
 p_port_11 = parallel.ParallelPort(address='0x3FF8')
 p_port_12 = parallel.ParallelPort(address='0x3FF8')
-
-# --- Initialize components for Routine "Palatability" ---
-Palatability_Score = visual.Slider(win=win, name='Palatability_Score',
-    startValue=None, size=(1.0, 0.1), pos=(0, -0.2), units=None,
-    labels=("Worst thing I have ever tasted", "", "Not horrible but would not try again", "", "Pleasant tasting, would try again", "", "Best thing I have ever tasted"), ticks=(1, 2, 3, 4, 5, 6, 7), granularity=1.0,
-    style='rating', styleTweaks=(), opacity=None,
-    labelColor=[0.8, 0.8, 0.8], markerColor=[1.0, -1.0, 1.0], lineColor='White', colorSpace='rgb',
-    font='Open Sans', labelHeight=0.02,
-    flip=False, ori=0.0, depth=0, readOnly=False)
-p_port_17 = parallel.ParallelPort(address='0x3FF8')
 
 # --- Initialize components for Routine "Rest" ---
 rest_text = visual.TextBox2(
@@ -1326,6 +1333,110 @@ for thisBlocks_of_Tastant in Blocks_of_Tastants:
         else:
             routineTimer.addTime(-12.100000)
         
+        # --- Prepare to start Routine "Palatability" ---
+        continueRoutine = True
+        routineForceEnded = False
+        # update component parameters for each repeat
+        Palatability_Score.reset()
+        # keep track of which components have finished
+        PalatabilityComponents = [Palatability_Score, p_port_17, Instruction]
+        for thisComponent in PalatabilityComponents:
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        frameN = -1
+        
+        # --- Run Routine "Palatability" ---
+        while continueRoutine:
+            # get current time
+            t = routineTimer.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=routineTimer)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            
+            # *Palatability_Score* updates
+            if Palatability_Score.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                Palatability_Score.frameNStart = frameN  # exact frame index
+                Palatability_Score.tStart = t  # local t and not account for scr refresh
+                Palatability_Score.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(Palatability_Score, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.timestampOnFlip(win, 'Palatability_Score.started')
+                Palatability_Score.setAutoDraw(True)
+            
+            # Check Palatability_Score for response to end routine
+            if Palatability_Score.getRating() is not None and Palatability_Score.status == STARTED:
+                continueRoutine = False
+            # *p_port_17* updates
+            if p_port_17.status == NOT_STARTED and t >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                p_port_17.frameNStart = frameN  # exact frame index
+                p_port_17.tStart = t  # local t and not account for scr refresh
+                p_port_17.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(p_port_17, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.addData('p_port_17.started', t)
+                p_port_17.status = STARTED
+                win.callOnFlip(p_port_17.setData, int(26))
+            if p_port_17.status == STARTED:
+                # is it time to stop? (based on global clock, using actual start)
+                if tThisFlipGlobal > p_port_17.tStartRefresh + .1-frameTolerance:
+                    # keep track of stop time/frame for later
+                    p_port_17.tStop = t  # not accounting for scr refresh
+                    p_port_17.frameNStop = frameN  # exact frame index
+                    # add timestamp to datafile
+                    thisExp.addData('p_port_17.stopped', t)
+                    p_port_17.status = FINISHED
+                    win.callOnFlip(p_port_17.setData, int(0))
+            
+            # *Instruction* updates
+            if Instruction.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                Instruction.frameNStart = frameN  # exact frame index
+                Instruction.tStart = t  # local t and not account for scr refresh
+                Instruction.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(Instruction, 'tStartRefresh')  # time at next scr refresh
+                # add timestamp to datafile
+                thisExp.timestampOnFlip(win, 'Instruction.started')
+                Instruction.setAutoDraw(True)
+            
+            # check for quit (typically the Esc key)
+            if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+                core.quit()
+            
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                routineForceEnded = True
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in PalatabilityComponents:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+            
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+        
+        # --- Ending Routine "Palatability" ---
+        for thisComponent in PalatabilityComponents:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        Each_Tastant_Once.addData('Palatability_Score.response', Palatability_Score.getRating())
+        Each_Tastant_Once.addData('Palatability_Score.rt', Palatability_Score.getRT())
+        if p_port_17.status == STARTED:
+            win.callOnFlip(p_port_17.setData, int(0))
+        # the Routine "Palatability" was not non-slip safe, so reset the non-slip timer
+        routineTimer.reset()
+        
         # --- Prepare to start Routine "Rinse" ---
         continueRoutine = True
         routineForceEnded = False
@@ -1531,99 +1642,6 @@ for thisBlocks_of_Tastant in Blocks_of_Tastants:
             routineTimer.reset()
         else:
             routineTimer.addTime(-39.200000)
-        
-        # --- Prepare to start Routine "Palatability" ---
-        continueRoutine = True
-        routineForceEnded = False
-        # update component parameters for each repeat
-        Palatability_Score.reset()
-        # keep track of which components have finished
-        PalatabilityComponents = [Palatability_Score, p_port_17]
-        for thisComponent in PalatabilityComponents:
-            thisComponent.tStart = None
-            thisComponent.tStop = None
-            thisComponent.tStartRefresh = None
-            thisComponent.tStopRefresh = None
-            if hasattr(thisComponent, 'status'):
-                thisComponent.status = NOT_STARTED
-        # reset timers
-        t = 0
-        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
-        frameN = -1
-        
-        # --- Run Routine "Palatability" ---
-        while continueRoutine:
-            # get current time
-            t = routineTimer.getTime()
-            tThisFlip = win.getFutureFlipTime(clock=routineTimer)
-            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
-            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
-            # update/draw components on each frame
-            
-            # *Palatability_Score* updates
-            if Palatability_Score.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                Palatability_Score.frameNStart = frameN  # exact frame index
-                Palatability_Score.tStart = t  # local t and not account for scr refresh
-                Palatability_Score.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(Palatability_Score, 'tStartRefresh')  # time at next scr refresh
-                # add timestamp to datafile
-                thisExp.timestampOnFlip(win, 'Palatability_Score.started')
-                Palatability_Score.setAutoDraw(True)
-            
-            # Check Palatability_Score for response to end routine
-            if Palatability_Score.getRating() is not None and Palatability_Score.status == STARTED:
-                continueRoutine = False
-            # *p_port_17* updates
-            if p_port_17.status == NOT_STARTED and t >= 0.0-frameTolerance:
-                # keep track of start time/frame for later
-                p_port_17.frameNStart = frameN  # exact frame index
-                p_port_17.tStart = t  # local t and not account for scr refresh
-                p_port_17.tStartRefresh = tThisFlipGlobal  # on global time
-                win.timeOnFlip(p_port_17, 'tStartRefresh')  # time at next scr refresh
-                # add timestamp to datafile
-                thisExp.addData('p_port_17.started', t)
-                p_port_17.status = STARTED
-                win.callOnFlip(p_port_17.setData, int(26))
-            if p_port_17.status == STARTED:
-                # is it time to stop? (based on global clock, using actual start)
-                if tThisFlipGlobal > p_port_17.tStartRefresh + .1-frameTolerance:
-                    # keep track of stop time/frame for later
-                    p_port_17.tStop = t  # not accounting for scr refresh
-                    p_port_17.frameNStop = frameN  # exact frame index
-                    # add timestamp to datafile
-                    thisExp.addData('p_port_17.stopped', t)
-                    p_port_17.status = FINISHED
-                    win.callOnFlip(p_port_17.setData, int(0))
-            
-            # check for quit (typically the Esc key)
-            if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
-                core.quit()
-            
-            # check if all components have finished
-            if not continueRoutine:  # a component has requested a forced-end of Routine
-                routineForceEnded = True
-                break
-            continueRoutine = False  # will revert to True if at least one component still running
-            for thisComponent in PalatabilityComponents:
-                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                    continueRoutine = True
-                    break  # at least one component has not yet finished
-            
-            # refresh the screen
-            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
-                win.flip()
-        
-        # --- Ending Routine "Palatability" ---
-        for thisComponent in PalatabilityComponents:
-            if hasattr(thisComponent, "setAutoDraw"):
-                thisComponent.setAutoDraw(False)
-        Each_Tastant_Once.addData('Palatability_Score.response', Palatability_Score.getRating())
-        Each_Tastant_Once.addData('Palatability_Score.rt', Palatability_Score.getRT())
-        if p_port_17.status == STARTED:
-            win.callOnFlip(p_port_17.setData, int(0))
-        # the Routine "Palatability" was not non-slip safe, so reset the non-slip timer
-        routineTimer.reset()
         
         # --- Prepare to start Routine "Rest" ---
         continueRoutine = True
