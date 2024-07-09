40f6b7857d58823633b2c89443059f2114c1753e
Diane Rohrer <120501144+DianeRohrer@users.noreply.github.com>
2024-07-09 11:59:10 -0400

    8827

--- MoralCondition_v2_lastrun.py
+++ MoralCondition_v2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on July 08, 2024, at 19:32
+    on July 09, 2024, at 10:24
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
