3ddba4d38fcf9528d12f60dbbb96bba272956e68
Diane Rohrer <120501144+DianeRohrer@users.noreply.github.com>
2025-01-30 16:52:28 -0500

    8768

--- Visual_Condition_v2_lastrun.py
+++ Visual_Condition_v2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on December 10, 2024, at 12:47
+    on January 30, 2025, at 15:17
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
