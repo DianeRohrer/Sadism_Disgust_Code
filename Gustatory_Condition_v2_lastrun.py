3de076e34d8bf9f38144098ca7b21e2c4e18d413
Diane Rohrer <120501144+DianeRohrer@users.noreply.github.com>
2025-02-25 18:13:18 -0500

    2760

--- Gustatory_Condition_v2_lastrun.py
+++ Gustatory_Condition_v2_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on February 24, 2025, at 09:40
+    on February 25, 2025, at 16:47
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
